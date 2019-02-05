7386f9a5a0738c2ff4f1feef0ac5b391ad741537
Dawid Kozakiewicz <turgor1996@gmail.com>
2019-02-05 10:10:22 +0100

    modification in require-own-files

--- n-3-3-require-own-files/app.js
+++ n-3-3-require-own-files/app.js
@@ -2,9 +2,9 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
-const notes = require('./notes.js');
-
-console.log('Result:', notes.add(9, -2));
+const fiut = require('./notes.js');
+console.log(fiut.add)
+console.log('Result:', fiut.add(9, -2));
 
 // var user = os.userInfo();
 //
